65264272ec0207f6d9c9227e9d037ea0ba4fd77f
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2025-07-31 19:28:54 -0300

    cinco

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.4023308052606!2d-61.581367900000004!3d-32.8081032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95c9e9bf85084001%3A0xe95051ce15c3f496!2sLa%20Fermina%20Club%20De%20Campo.!5e0!3m2!1ses-419!2sar!4v1715292113659!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3143.5505628405435!2d-59.261015699999994!3d-38.010942400000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9591c951a57ca687%3A0xa988646de4167f4f!2sLa%20Roca%20Disco!5e0!3m2!1ses-419!2sar!4v1754000815553!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         
         
     })
